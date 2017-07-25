85c58a32a92c04df644bfea327e953fc9f25d2d8
Devkota <info@devkotasagar.com.np>
2017-07-25 20:49:39 +0545

    initial added

---
+++ classmates/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+# class Class(models.Model):
+#     name = models.CharField(max_length=50)
+#     slug = models.SlugField(max_length=20, unique=True)
+#     dob = models.DateField()
+#     GENDER = (
+#     ('0', 'Male'),
+#     ('1', 'Female'),
+#     ('3', 'Other'),
+#     )
+#     gender = models.CharField(max_length=1, choices=GENDER)
+#
+#     def __str__ (self):
+#         return self.name
