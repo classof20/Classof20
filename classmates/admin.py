be24d7c02cb981181834f9fba83fae7901033dc5
Devkota <info@devkotasagar.com.np>
2017-10-20 07:08:16 +0545

    Adding mambers no css :)

--- classmates/admin.py
+++ classmates/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Mamber
+
+class MamberAdmin(admin.ModelAdmin):
+    list_display=("id","name")
+    prepopulated_fields = {"slug":("name",)}
+
+
+admin.site.register(Mamber, MamberAdmin)
--- classmates/views.py
+++ classmates/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404, redirect
 
 # Create your views here.
+from .models import Mamber
 
 def home(request):
     template_name = 'index.html'
     context = {'title':"Class of 20"}
+    return render(request,template_name,context)
+
+def mambers(request):
+    template_name = 'mambers.html'
+    infor = Mamber.objects.all()
+    context = {'title': 'Mambers','infor':infor}
+    return render(request,template_name,context)
+
+def mamber(request, slug):
+    template_name = 'mamber.html'
+    infor = get_object_or_404(Mamber, slug=slug)
+    context = {'title':infor.name,'infor':infor}
     return render(request,template_name,context)
